ff8121b6e0e2cdd367273a1b5b42d0a65b1761fb
iege <gerasumenkoeugene@gmail.com>
2018-02-28 16:55:46 +0200

    test commit for collaborator

--- src/main/java/com/taskombank/gerasimenko/SpringApplicationTwo.java
+++ src/main/java/com/taskombank/gerasimenko/SpringApplicationTwo.java
@@ -2,7 +2,6 @@ package com.taskombank.gerasimenko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringApplicationTwo {
@@ -10,4 +9,5 @@ public class SpringApplicationTwo {
     public static void main(String[] args) {
         SpringApplication.run(SpringApplicationTwo.class, args);
     }
+
 }
