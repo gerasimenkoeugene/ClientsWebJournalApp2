4642fdffc5a5d4479dc20caea839b735fe35644e
gerasimenkoeugene <gerasymenkoeugene@gmail.com>
2018-07-19 13:02:37 +0300

    convert artifact to war

--- src/main/java/com/taskombank/gerasimenko/SpringApplicationTwo.java
+++ src/main/java/com/taskombank/gerasimenko/SpringApplicationTwo.java
@@ -2,9 +2,10 @@ package com.taskombank.gerasimenko;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringApplicationTwo {
+public class SpringApplicationTwo extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringApplicationTwo.class, args);
